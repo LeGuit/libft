6b16b568e498fbd7f3d6a4f5bd3836049a308393
Guillaume WOODWARD <gwoodwar@e1r1p10.42.fr>
2015-11-26 12:12:18 +0100

    mmve

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 14:05:01 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/25 11:31:39 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/26 11:41:12 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	unsigned char	*tsrc;
+	unsigned char	*tdst;
+
+	tsrc = (unsigned char *)src;
+	tdst = (unsigned char *)dst;
 	if (dst <= src)
 		ft_memcpy(dst, src, len);
 	else
-		ft_memrcpy(dst, src, len);
+	{
+		while (len > 0)
+		{
+			*(tdst + len) = *(tsrc + len);
+			len--;
+		}
+	}
 	return (dst);
 }
