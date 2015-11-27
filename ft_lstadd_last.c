78e8bdb65a15fcb8432c95266aa122192244d034
Guillaume WOODWARD <gwoodwar@e1r5p21.42.fr>
2015-11-27 14:14:08 +0100

    up **alist

--- ft_lstadd_last.c
+++ ft_lstadd_last.c
@@ -6,20 +6,20 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 18:41:57 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/24 11:34:33 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/27 14:13:52 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_last(t_list *alst, t_list *new)
+void	ft_lstadd_last(t_list **alst, t_list *new)
 {
-	if (alst == NULL)
-		alst = new;
+	if (*alst == NULL)
+		*alst = new;
 	else
 	{
-		while (alst->next)
-			alst = alst->next;
-		alst = new;
+		while (*alst->next)
+			*alst = *alst->next;
+		*alst = new;
 	}
 }
