ae700b460b64f25d1858206feca8e91cb4b40681
Guillaume WOODWARD <gwoodwar@e1r5p21.42.fr>
2015-11-27 11:50:50 +0100

    up lstfind

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 09:43:55 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/27 11:19:32 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/27 11:49:51 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,5 +109,6 @@ void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstadd_last(t_list *alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list				*ft_lstfind(t_list *blist, void *ref, int (*cmp)());
 
 #endif
