c20b53f1d69c8b641bc17d10cce17ce68c43dcf0
Guillaume WOODWARD <gwoodwar@e1r5p21.42.fr>
2015-11-27 14:14:40 +0100

    up **alst add_last

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 09:43:55 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/27 11:49:51 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/27 14:14:20 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
-void				ft_lstadd_last(t_list *alst, t_list *new);
+void				ft_lstadd_last(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 t_list				*ft_lstfind(t_list *blist, void *ref, int (*cmp)());
