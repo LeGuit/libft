a535a630de59d1babe8d7de153a8786066f8b573
Guillaume WOODWARD <gwoodwar@e1r1p10.42.fr>
2015-11-30 17:57:58 +0100

    mmove better strealloc == join

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 14:05:01 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/26 13:20:11 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/30 17:56:56 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*tsrc;
-	unsigned char	*tdst;
-
-	tsrc = (unsigned char *)src;
-	tdst = (unsigned char *)dst;
 	if (dst <= src)
 		ft_memcpy(dst, src, len);
 	else
