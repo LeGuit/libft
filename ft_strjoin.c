56b41bc43e0a5ef5fd225e77ec5b348298538dd3
Guillaume WOODWARD <gwoodwar@e1r5p19.42.fr>
2015-12-02 10:42:46 +0100

    case no s1 || no s2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 17:31:03 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/24 12:43:36 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/02 10:41:54 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	size_t	i;
 
+	if (!s1 || !s2)
+		return (ft_strdup((s1) ? s1 : s2));
 	res = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1);
 	i = 0;
 	while (i < ft_strlen(s1))
