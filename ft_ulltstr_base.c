de93528a35daeb74a8559c033a6e2fe18ec9c4f8
Guillaume WOODWARD <gwoodwar@e1r10p7.42.fr>
2015-12-15 19:23:44 +0100

    norme

--- ft_ulltstr_base.c
+++ ft_ulltstr_base.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 17:12:43 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/15 18:19:27 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/15 19:22:42 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ char		*ft_ulltstr_base(unsigned long long n, char *base_str, char *buf)
 	}
 	base = ft_strlen(base_str);
 	size = len_str(n, base);
-	buf[size + 1] = 0;		
+	buf[size + 1] = 0;
 	while (n)
 	{
 		buf[size] = base_str[n % base];
