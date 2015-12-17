87ff0f10e0138fae31bf90484c8155fa4bfee912
Guillaume WOODWARD <gwoodwar@e1r5p17.42.fr>
2015-12-17 17:44:55 +0100

    widetoa in print_c to test

--- print_c.c
+++ print_c.c
@@ -6,37 +6,39 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 15:27:45 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/16 15:41:48 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/17 17:44:19 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 #include <wchar.h>
 
-static char		get_arg_c(t_mod *m, va_list ap)
+static int		get_arg_c(t_mod *m, va_list ap)
 {
 	if (GET(m->modif, MOD_L))
-		return ((wint_t)va_arg(ap, int));
+		return ((int)(wint_t)va_arg(ap, int));
 	else
-		return ((char)va_arg(ap, int));
+		return ((int)(char)va_arg(ap, int));
 }
 
 int				print_c(t_mod *m, va_list ap)
 {
 	size_t		cnt;
-	char		arg;
+	int			arg;
+	char		buf[5];
 
 	arg = get_arg_c(m, ap);
+	ft_widetoa(buf, 5, arg);
 	cnt = 1;
 	if (GET(m->flag, F_MINUS))
 	{
-		ft_putchar(arg);
+		ft_putstr(buf);
 		cnt += ft_space(m, cnt);
 	}
 	else
 	{
 		cnt += ft_space(m, cnt);
-		ft_putchar(arg);
+		ft_putstr(buf);
 	}
 	return (cnt);
 }
