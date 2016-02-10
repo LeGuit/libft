26714450d33533e1a2ba7d0550e155c8872068a3
Guillaume WOODWARD <gwoodwar@e3r3p2.42.fr>
2016-02-10 12:02:06 +0100

    256 buf for Gnl

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 10:48:00 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/02/10 11:48:46 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/02/10 12:01:42 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _GET_NEXT_LINE_H
 # define _GET_NEXT_LINE_H
 
-# define BUFF_SIZE			10000
+# define BUFF_SIZE			256
 # define CONTENT(x)			((t_out*)(x)->content)
 
 # include "libft.h"
