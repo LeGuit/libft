fc9aed8464fb68e6309ce458435e5de91cfad8b0
Guillaume WOODWARD <gwoodwar@e1r2p17.42.fr>
2016-03-14 14:27:28 +0100

    save draw

--- includes/ft_draw.h
+++ includes/ft_draw.h
@@ -1,5 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_draw.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/14 14:23:42 by gwoodwar          #+#    #+#             */
+/*   Updated: 2016/03/14 14:24:45 by gwoodwar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef _FT_DRAW_H
 # define _FT_DRAW_H
+
+typedef struct		s_vec3i
+{
+	int				x;
+	int				y;
+	int				z;
+}					t_vec3i;
+
+typedef struct		s_image
+{
+	void			*ptr;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	char			*data;
+	int				width;
+	int				height;
+}					t_image;
 
 void				ft_put_pix_to_img(t_vec3i *v, t_image *i);
 int					ft_mlx_image_init(void *mlx_ptr, t_image *image, int width,
