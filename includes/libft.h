cd9c0817d569d431bc7d5ab92a5e814eac1430fe
Guillaume WOODWARD <gwoodwar@e3r4p15.42.fr>
2016-08-02 15:20:04 +0200

    add dlst.h in libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,25 +6,26 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 09:43:55 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/02/15 17:47:54 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/08/02 14:06:04 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _LIBFT_H
 # define _LIBFT_H
 
-# include "ft_printf.h"
-# include "get_next_line.h"
+# include "ft_color.h"
+# include "ft_dlst.h"
+# include "ft_draw.h"
 # include "ft_is.h"
 # include "ft_lsts.h"
+# include "ft_macro.h"
 # include "ft_mem.h"
+# include "ft_printf.h"
 # include "ft_puts.h"
 # include "ft_str.h"
 # include "ft_tabs.h"
 # include "ft_vect.h"
 # include "ft_xtoy.h"
-# include "ft_macro.h"
-# include "ft_color.h"
-# include "ft_draw.h"
+# include "get_next_line.h"
 
 #endif
